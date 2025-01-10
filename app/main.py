9b4f01ea4b78a19138d836402b541a5c00ece985
eaverdeja <eaverdeja@gmail.com>
2025-01-10 14:28:54 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 import sys
+import os
 
 BUILTIN_COMMANDS = [
     "echo",
     "exit",
     "type",
 ]
+
+def list_executables(path):
+    executables = []
+    if os.path.exists(path) and os.path.isdir(path):
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            # Check if file is executable
+            if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                executables.append(file)
+    return executables
+
 
 def main():
     sys.stdout.write("$ ")
@@ -18,7 +30,17 @@ def main():
             if word in BUILTIN_COMMANDS:
                 sys.stdout.write(f"{word} is a shell builtin\n")
             else:
-                sys.stdout.write(f"{word}: not found\n")
+                path_list = os.environ['PATH'].split(os.pathsep)
+                path_for_word = None
+                for path in path_list:
+                    executables = list_executables(path)
+                    if word in executables:
+                        path_for_word = path + f"/{word}"
+                
+                if path_for_word:
+                    sys.stdout.write(f"{word} is {path_for_word}\n")
+                else:
+                    sys.stdout.write(f"{word}: not found\n")
         case "echo":
             words = pieces[1:]
             sys.stdout.write(" ".join(words) + "\n")
