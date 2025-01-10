775e6bb61d566a7e5a669bd05afe2349f996fa5b
eaverdeja <eaverdeja@gmail.com>
2025-01-10 12:17:50 -0300

    Handling the type command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import sys
 
+BUILTIN_COMMANDS = [
+    "echo",
+    "exit",
+    "type",
+]
 
 def main():
     sys.stdout.write("$ ")
@@ -8,6 +13,12 @@ def main():
      
     pieces = command.split(" ")
     match pieces[0]:
+        case "type":
+            word = pieces[1]
+            if word in BUILTIN_COMMANDS:
+                sys.stdout.write(f"{word} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{word}: not found\n")
         case "echo":
             words = pieces[1:]
             sys.stdout.write(" ".join(words) + "\n")
